c66006fe276bd83a9fe57d72e9698f6f608078df
muski-gupta <musku.2200@gmail.com>
2026-08-03 15:37:09 +0530

    all asded

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo {
     public static void main(String [] args){
         System.out.print("Hello world");
+        System.out.print("Hello world");
+        System.out.print("Hello world");
     }
 }
