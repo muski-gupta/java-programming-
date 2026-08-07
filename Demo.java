0bd120266f6c7acb667e8af23cc6102fa6ab0a73
muski-gupta <musku.2200@gmail.com>
2026-08-07 15:00:48 +0530

    first code

--- Demo.java
+++ Demo.java
@@ -1,7 +1,18 @@
 public class Demo {
+    static int x=10;
     public static void main(String [] args){
+
         System.out.print("Hello world");
         System.out.print("Hello world");
         System.out.print("Hello world");
+        Demo2 obj = new Demo2();
+        obj.display();
     }
 }
+class Demo2{
+        void display(){
+            Demo.x=10;
+            System.out.println(Demo.x);
+    }
+}
+
