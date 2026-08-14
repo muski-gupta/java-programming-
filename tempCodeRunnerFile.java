3b3eafc4e6003fea5fc3b82acc631e3f240c71b4
muski-gupta <musku.2200@gmail.com>
2026-08-14 15:15:24 +0530

    CLi

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+public class CLI {
+//     public static void main (String [] args){
+//         System.out.println("=== Deployment Health v1.0 ===");
+//         System.out.println("Arguments received : " + args.length);
+
+//      if(args.length == 0){
+//         System.out.println("Usage : java P01 <service> <port>");
+//         return ;
+//      }
+//      for(int i=0;i<args.length;i++){
+      
+//         System.err.println("args [ " + i  + " ] = " + args[i]);
+//      }
+//      String service = args[0];
+//      int port =Integer.parseInt(args[1]);
+//      System.out.println(" Pinging " + service + " on port " + port + " ...Ok ");
+
+//     }
+// }
